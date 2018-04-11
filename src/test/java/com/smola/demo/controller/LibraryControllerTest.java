5f92afd30e1042d4669ff833a07ab6dafe5e568a
Marcin Smola <Marcin_Smola@epam.com>
2018-04-11 16:43:32 +0200

    refactored IT tests, need to add finding by id feature

--- src/test/java/com/smola/demo/controller/LibraryControllerTest.java
+++ src/test/java/com/smola/demo/controller/LibraryControllerTest.java
@@ -7,12 +7,14 @@ import com.smola.demo.model.library.ISBN;
 import com.smola.demo.model.library.Tittle;
 import com.smola.demo.repository.BookRepository;
 import com.smola.demo.service.LibraryService;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -32,9 +34,13 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @WebAppConfiguration
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class LibraryControllerTest {
-    private MockMvc mockMvc;
     private static final String endPoint = "/library/books";
+    private static final String SAMPLE_AUTHOR_NAME = "Adam Mickiewicz";
+    private static final String SAMPLE_BOOK_TITTLE = "Pan Tadeusz";
+    private static final String SAMPLE_BOOK_ISBN = "978-1-56619-909-4";
+    private MockMvc mockMvc;
     @Autowired
     private LibraryService libraryService;
 
@@ -56,7 +62,6 @@ public class LibraryControllerTest {
 
     @Test
     public void should_ReturnHttp200_whenGetOnAllBooks() throws Exception {
-        String endPoint = "/library/books";
         this.mockMvc.perform(get(endPoint))
                 .andExpect(status().isOk());
     }
@@ -64,11 +69,7 @@ public class LibraryControllerTest {
     @Test
     public void should_ReturnHttp201_whenPostNewBook() throws Exception {
         //given
-        Author author = new Author("Adam Mickiewicz");
-        Tittle tittle = new Tittle("Pan Tadeusz");
-        ISBN isbn = new ISBN("978-1-56619-909-4 ");
-
-        Book bookToAdd = new Book.BookBuilder(author, tittle).setIsbn(isbn).build();
+        Book bookToAdd = createSampleBook();
         Gson gson = new Gson();
         String bookToAddJSON = gson.toJson(bookToAdd);
         //when - then
@@ -80,11 +81,7 @@ public class LibraryControllerTest {
     @Test
     public void should_AddBookToRepository_whenPostNewBook() throws Exception {
         //given
-        Author author = new Author("Adam Mickiewicz");
-        Tittle tittle = new Tittle("Pan Tadeusz");
-        ISBN isbn = new ISBN("978-1-56619-909-4 ");
-
-        Book bookToAdd = new Book.BookBuilder(author, tittle).setIsbn(isbn).build();
+        Book bookToAdd = createSampleBook();
         Gson gson = new Gson();
         String bookToAddJSON = gson.toJson(bookToAdd);
         //when
@@ -93,13 +90,34 @@ public class LibraryControllerTest {
         //then
         this.mockMvc.perform(get(endPoint))
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].author.name", is(SAMPLE_AUTHOR_NAME)))
                 .andExpect(jsonPath("$", hasSize(1)));
     }
 
     @Test
-    public void should_ReturnBook_whenGetById(){
+    public void should_ReturnBook_whenGetById() throws Exception {
         //given
-        int idToDelete = 0;
-//        String endPointToGetBookById = String.format(endPoint+"/%s",);
+        int idToGet = 1;
+        String endPointToGetBookById = String.format(endPoint + "/%s", idToGet);
+
+        //when
+        Book bookToAdd = createSampleBook();
+        Gson gson = new Gson();
+        String bookToAddJSON = gson.toJson(bookToAdd);
+        //when
+        this.mockMvc.perform(post(endPoint).contentType(contentType).content(bookToAddJSON));
+
+        //then
+        this.mockMvc.perform(get(endPointToGetBookById))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].author.name", is(SAMPLE_AUTHOR_NAME)));
+    }
+
+    private Book createSampleBook() {
+        Author author = new Author(SAMPLE_AUTHOR_NAME);
+        Tittle tittle = new Tittle(SAMPLE_BOOK_TITTLE);
+        ISBN isbn = new ISBN(SAMPLE_BOOK_ISBN);
+        return new Book.BookBuilder(author, tittle).setIsbn(isbn).build();
     }
 }
